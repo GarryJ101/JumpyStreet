d4a91f1f77f49d6d91dfac95dad1f13b23bec445
GarryJ101 <gmjohnston@my.waketech.edu>
2021-09-13 21:20:49 -0400

    Terrain
    Terrain procedurally generated but no player so doesn't work yet.

--- JumpyStreet/Assets/Scripts/TerrainGenerator.cs
+++ JumpyStreet/Assets/Scripts/TerrainGenerator.cs
@@ -6,5 +6,63 @@ public class TerrainGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject grassTile;
     [SerializeField] private GameObject roadTile;
-    [SerializeField] private GameObject WaterTile;
+    [SerializeField] private GameObject waterTile;
+
+    int firstRand;
+    int secondRand;
+    int disPlayer = 12;
+
+    Vector3 intPos = new Vector3(0, 0, 0);
+
+    private void Update()
+    {
+        if (Input.GetButtonDown ("up"))
+        {
+            firstRand = Random.Range(1, 4);
+            if (firstRand == 1)
+            {
+                secondRand = Random.Range(1, 6);
+
+                for(int i = 0; i< secondRand; i++)
+                {
+                    intPos = new Vector3(0, 0, disPlayer);
+                    disPlayer += 1;
+                    GameObject GrassIns = Instantiate(grassTile) as GameObject;
+                    GrassIns.transform.position = intPos;
+                }
+            }
+        }
+        {
+            firstRand = Random.Range(1, 4);
+            if (firstRand == 2)
+            {
+                secondRand = Random.Range(1, 6);
+
+                for (int i = 0; i < secondRand; i++)
+                {
+                    intPos = new Vector3(0, 0, disPlayer);
+                    disPlayer += 1;
+                    GameObject RoadIns = Instantiate(roadTile) as GameObject;
+                    RoadIns.transform.position = intPos;
+                }
+            }
+        }
+        {
+            firstRand = Random.Range(1, 4);
+            if (firstRand == 3)
+            {
+                secondRand = Random.Range(1, 6);
+
+                for (int i = 0; i < secondRand; i++)
+                {
+                    intPos = new Vector3(0, 0, disPlayer);
+                    disPlayer += 1;
+                    GameObject WaterIns = Instantiate(waterTile) as GameObject;
+                    WaterIns.transform.position = intPos;
+                }
+            }
+        }
+    }
+
+
 }
