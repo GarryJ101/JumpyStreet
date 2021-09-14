48fe7c7ada66d830c5252a346ffe984a55ac2bdc
GarryJ101 <gmjohnston@my.waketech.edu>
2021-09-13 20:05:50 -0400

    initial commit MOD
    New initial commit with Unity project

---
+++ JumpyStreet/Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject grassTile;
+    [SerializeField] private GameObject roadTile;
+    [SerializeField] private GameObject WaterTile;
+}
